e0c58cb03a5b29167b9fd8c3672b1bbad8822769
Ashlyn <Amcintosh8@ggc.edu>
2023-01-26 08:30:21 -0500

    Add Chocolate.java

---
+++ src/Chocolate.java
@@ -0,0 +1,10 @@
+/**
+ * @Class Name: Chocolate
+ * @Author: Ashlyn McIntosh
+ * @Version: 1.0
+ * @Date: 26 January 2023
+ * Description: A program designed to count the number of bags needed to get 3 chocolates instead of two
+ */
+
+public class Chocolate {
+}
---
+++ src/SpeedLight.java
@@ -0,0 +1,21 @@
+/**
+ * @Class Name: SpeedLight
+ * @Author: Ashlyn McIntosh
+ * @Version: 1.0
+ * @Date: 26 January 2023
+ * Description: A program designed to calculate the speed of light
+ */
+
+
+
+
+
+
+
+public class SpeedLight {
+}
+    public static void main{(String[]args){
+        //edited
+
+    }
+        }
